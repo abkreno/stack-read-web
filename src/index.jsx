094a115c1ece34eaf8306c86b37f990e71fc16f7
Abkreno <mohamedelhamamsy789@gmail.com>
2018-09-08 03:49:35 +0200

    Add redux router

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../componentns/App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
 registerServiceWorker();
