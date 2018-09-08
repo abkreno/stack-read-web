58d216046863432db8eeeaea187acf230d8f39a5
Abkreno <mohamedelhamamsy789@gmail.com>
2018-09-08 05:08:31 +0200

    Add antd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
